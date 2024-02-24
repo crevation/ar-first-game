f2a79889c04a02c7a08507cd2afd809ba694de8d
ishihaya <ishidahayate49@gmail.com>
2024-02-24 14:52:14 +0900

    first commit

---
+++ ar-first-game/ar_first_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_first_gameApp.swift
+//  ar-first-game
+//
+//  Created by ishihaya on 2024/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ar_first_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
