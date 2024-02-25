e87a80b0348baa6b44b86fa27b7adba40a8feee2
ishihaya <ishidahayate49@gmail.com>
2024-02-25 14:52:51 +0900

    rename

--- argame01/ARGame01App.swift
+++ argame01/ARGame01App.swift
@@ -1,5 +1,5 @@
 //
-//  ARFirstGameApp.swift
+//  ARGame01App.swift
 //  ar-first-game
 //
 //  Created by ishihaya on 2024/02/24.
@@ -10,7 +10,7 @@ import SwiftUI
 //import RealityKitContent
 
 @main
-struct ARFirstGameApp: App {
+struct ARGame01App: App {
     @State private var currentStyle: ImmersionStyle = .full
 
     var body: some Scene {
--- argame01Tests/ar_first_gameTests.swift
+++ argame01Tests/ar_first_gameTests.swift
@@ -8,7 +8,7 @@
 @testable import ar_first_game
 import XCTest
 
-class ARFirstGameAppTests: XCTestCase {
+class ARGame01AppTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
